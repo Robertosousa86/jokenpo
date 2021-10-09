9f407dd46c492ee011eadf72f47d5881f043fdb6
Roberto Sousa <robertosousa186@gmail.com>
2021-10-09 03:42:58 -0300

    Creating index.js to start the app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
-
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+module.exports = app;
