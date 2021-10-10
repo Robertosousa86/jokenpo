3f07da84dc74068fa85efd5d6166d37cf2a44c74
Roberto Sousa <robertosousa186@gmail.com>
2021-10-10 00:59:04 -0300

    Creating the second test case

--- __tests__/Jokenpo.spec.js
+++ __tests__/Jokenpo.spec.js
@@ -4,4 +4,8 @@ describe('JokenpoService', () => {
   it('should be return `pedra` if the value of key is 0', () => {
     expect(jokenpo(0)).toBe('pedra');
   });
+
+  it('should be return `papel` if the value of key is 1', () => {
+    expect(jokenpo(1)).toBe('papel');
+  });
 });
