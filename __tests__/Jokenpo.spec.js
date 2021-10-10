533713343658132804acad6e6ddbbdb8a573c1e3
Roberto Sousa <robertosousa186@gmail.com>
2021-10-10 01:19:16 -0300

    Fix test description

--- __tests__/Jokenpo.spec.js
+++ __tests__/Jokenpo.spec.js
@@ -1,17 +1,17 @@
-const jokenpo = require('../src/controller/JokenpoService');
+const JokenpoService = require('../src/controller/JokenpoService');
 
 describe('JokenpoService', () => {
   it('should be return `pedra` if the value of key is 0', () => {
-    expect(jokenpo(0)).toBe('pedra');
+    expect(JokenpoService.jokenpo(0)).toBe('pedra');
   });
 
   it('should be return `papel` if the value of key is 1', () => {
-    expect(jokenpo(1)).toBe('papel');
+    expect(JokenpoService.jokenpo(1)).toBe('papel');
   });
 
   it('should be return `tesoura` if the value of key is 2', () => {
-    expect(jokenpo(2)).toBe('tesoura');
+    expect(JokenpoService.jokenpo(2)).toBe('tesoura');
   });
 
-  it('should be return a random nunber between 0 and 2', () => {});
+  it('should be return a random number between 0 and 2', () => {});
 });
