4189d2c74f7df79798002140c277c5fda6ebc851
Roberto Sousa <robertosousa186@gmail.com>
2021-10-11 02:53:34 -0300

    Three first tests  Passing

--- __tests__/Jokenpo.spec.js
+++ __tests__/Jokenpo.spec.js
@@ -1,7 +1,15 @@
 const JokenpoService = require('../src/controller/JokenpoService');
 
 describe('JokenpoService', () => {
+  it('should be return 0 when the key is 0', () => {
+    expect(JokenpoService.jokenpo(0)).toBe(0);
+  });
+
   it('should be return 1 when the key is 1', () => {
     expect(JokenpoService.jokenpo(1)).toBe(1);
+  });
+
+  it('should be return 2 when the key is 2', () => {
+    expect(JokenpoService.jokenpo(2)).toBe(2);
   });
 });
