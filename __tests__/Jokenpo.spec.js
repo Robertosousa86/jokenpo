2a3d29b87ed1dc5aa63482403cc775e568e39b58
Roberto Sousa <robertosousa186@gmail.com>
2021-10-11 02:35:13 -0300

    Fist stage:Fail

--- __tests__/Jokenpo.spec.js
+++ __tests__/Jokenpo.spec.js
@@ -1,3 +1,5 @@
 describe('JokenpoService', () => {
-  it('should be return 1 when the key is 1', () => {});
+  it('should be return 1 when the key is 1', () => {
+    expect(JokenpoService.jokenpo(1)).toBe(1);
+  });
 });
