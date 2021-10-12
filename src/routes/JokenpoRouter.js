e9dc3adb13b7b589d8e9a39b44fd70337083e921
Roberto Sousa <robertosousa186@gmail.com>
2021-10-12 01:10:55 -0300

    Creating route post

--- src/routes/JokenpoRouter.js
+++ src/routes/JokenpoRouter.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const JokenpoService = require('../controller/JokenpoService');
 
 router.get('/', (req, res) => {
   res.send('JOKENPO!');
+});
+
+router.post('/', (req, res) => {
+  const { key } = req.body;
+  const robot = JokenpoService.jokenbot();
+
+  return res.send(JokenpoService.game(key, robot));
 });
 
 module.exports = router;
