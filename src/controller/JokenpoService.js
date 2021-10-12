4d96b4b7833d40b6d31df758b5cc4030993ae27d
Roberto Sousa <robertosousa186@gmail.com>
2021-10-11 22:29:51 -0300

    Implementing game function

--- src/controller/JokenpoService.js
+++ src/controller/JokenpoService.js
@@ -17,6 +17,26 @@ const robot = jokenbot();
 function game(key, robotKey = robot) {
   if (jokenpo(key) === 0 && robotKey === 0) {
     return { message: 'Ops! Empate!' };
+  } else if (jokenpo(key) === 0 && robotKey === 2) {
+    return { message: 'Wow! Você venceu!' };
+  } else if (jokenpo(key) === 0 && robotKey === 1) {
+    return { message: 'Heyta... Robot venceu!' };
+  }
+
+  if (jokenpo(key) === 1 && robotKey === 1) {
+    return { message: 'Empate' };
+  } else if (jokenpo(key) === 1 && robotKey === 0) {
+    return { message: 'Wow! Você venceu!' };
+  } else if (jokenpo(key) === 1 && robotKey === 2) {
+    return { message: 'Heyta... Robot venceu!' };
+  }
+
+  if (jokenpo(key) === 2 && robotKey === 2) {
+    return { message: 'Empate' };
+  } else if (jokenpo(key) === 2 && robotKey === 1) {
+    return { message: 'Wow! Você venceu!' };
+  } else if (jokenpo(key) === 2 && robotKey === 0) {
+    return { message: 'Heyta... Robot venceu!' };
   }
 }
 
