3e98effc12f7be5b19b73da6a8641c67f9a000f2
Roberto Sousa <robertosousa186@gmail.com>
2021-10-12 21:43:52 -0300

    Adding cors and static

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const JokenpoRouter = require('./routes/JokenpoRouter');
 
+app.use(cors());
+app.use(express.static('www'));
 app.use(express.json());
 
 app.use(JokenpoRouter);
